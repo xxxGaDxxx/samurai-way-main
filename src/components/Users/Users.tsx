bfa151ebc4dd46c42a126b169deb042e917704c7
Wlad loban <qwegadqwe@gmail.com>
2022-06-23 13:12:03 +0300

    54. жизненный цикл, life cycle methods, componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -11,57 +11,21 @@ type UsersPropsType = {
     setUser: (users: UsersType[]) => void
 }
 
-export const Users: React.FC<UsersPropsType> = (props) => {
+export class Users extends React.Component<UsersPropsType> {/////////////////////////
 
-    let getUssers = () => {
-
-
-        if (props.users.length === 0) {
-
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                props.setUser(response.data.items)
-            })
-            /*props.setUser([
-                {
-                    id: 1,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdDKH8Ew3p9hw0I9QKHFDP58aWZ-d6NUfHkA&usqp=CAU',
-                    followed: false,
-                    fullName: 'Wlad L.',
-                    status: 'НЕ СТАВИТЬ НОВЫЕ БИБЛИОТЕКИ!!!!',
-                    location: {citi: 'Ozery', contry: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGrTuyVvRpS-lz4Rg4jVaT9P7iKAx3T2vK5Q&usqp=CAU',
-                    followed: false,
-                    fullName: 'Margo B.',
-                    status: 'НЕ СТАВИТЬ НОВЫЕ БИБЛИОТЕКИ!!!!',
-                    location: {citi: 'Skidel', contry: 'Belarus'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRP5VKFOTn-2IxmSp9pcNC_B0PHDDvNQSAeVQ&usqp=CAU',
-                    followed: true,
-                    fullName: 'Radic R',
-                    status: 'НЕ СТАВИТЬ НОВЫЕ БИБЛИОТЕКИ!!!!',
-                    location: {citi: 'Grodno', contry: 'Belarus'}
-                },
-                {
-                    id: 4,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1KjoRsUfR-lyCcHd4_fbLsIDGLrVQKB6Iug&usqp=CAU',
-                    followed: true,
-                    fullName: 'Pascha S.',
-                    status: 'НЕ СТАВИТЬ НОВЫЕ БИБЛИОТЕКИ!!!!',
-                    location: {citi: 'Ozery', contry: 'Belarus'}
-                },])*/
-        }
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUser(response.data.items)
+        })
     }
 
-    return (
-        <div className={s.users}>
-            <button onClick={getUssers}>Get Users</button>
-            {
-                props.users.map(u => <div key={u.id}>
+    render() {
+        return (
+            <div className={s.users}>
+                {/*<button onClick={this.getUssers}>Get Users</button>*/}
+                {
+                    this.props.users.map((u:UsersType) =>/////////////////////
+                        <div key={u.id}>
                     <span>
                         <div>
                             <img src={userPhoto} alt={''}/> {/*u.photo.small !== null ? u.photo.small :userPhoto*/}
@@ -70,16 +34,16 @@ export const Users: React.FC<UsersPropsType> = (props) => {
                             {
                                 u.followed
                                     ? <button onClick={() => {
-                                        props.unfollow(u.id)
+                                        this.props.unfollow(u.id)
                                     }}>Unfollow</button>
                                     : <button onClick={() => {
-                                        props.follow(u.id)
+                                        this.props.follow(u.id)
                                     }}>Follow</button>
                             }
 
                         </div>
                     </span>
-                    <span>
+                            <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -89,9 +53,11 @@ export const Users: React.FC<UsersPropsType> = (props) => {
                             <div>{'u.location.citi'}</div>
                         </span>
                     </span>
-                </div>)
-            }
-        </div>
-    );
-};
+                        </div>)
+                }
+            </div>
+        );
+    }
+}
+
 
